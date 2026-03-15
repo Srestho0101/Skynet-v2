f1970baa76ae2beb109206ef5d9b5337e82d59a8
Srestho <srestho1298@gmail.com>
2026-03-15 06:49:00 +0600

    Updated run.py to change app.run(debug=True) to use gunicorn.

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import os
 from app import create_app, db
 
 load_dotenv()
-app = create_app(os.getenv('FLASK_ENV', 'development'))
+app = create_app(os.getenv('FLASK_ENV', 'production'))
 
 @app.shell_context_processor
 def make_shell_context():
@@ -18,4 +18,4 @@ def make_shell_context():
     }
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
